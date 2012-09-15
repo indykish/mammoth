704fa668102b5328f76f5427b69990587b617c68
Kishorekumar Neelamegam <indykish@gmail.com>
2012-09-15 09:57:41 +0530

    Fixed duplicate in pom.xml, removed imports in GoogleAppServiceImpl.(fix for comple errors)

--- src/main/java/org/megam/mammoth/prov/googleapps/service/GoogleAppsServiceImpl.java
+++ src/main/java/org/megam/mammoth/prov/googleapps/service/GoogleAppsServiceImpl.java
@@ -1,88 +1,28 @@
 package org.megam.mammoth.prov.googleapps.service;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.validation.Validator;
+
 import org.megam.mammoth.prov.info.GoogleUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.style.ToStringCreator;
 import org.springframework.stereotype.Service;
-import com.amazonaws.util.json.JSONArray;
-import com.amazonaws.util.json.JSONException;
-import com.amazonaws.util.json.JSONObject;
-import com.google.api.client.json.JsonEncoding;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.JsonGenerator;
-import com.google.api.client.json.JsonObjectParser;
-import com.google.api.client.json.JsonParser;
-import com.google.gdata.client.appsforyourdomain.AppsForYourDomainQuery;
+
 import com.google.gdata.client.appsforyourdomain.AppsGroupsService;
 import com.google.gdata.client.appsforyourdomain.EmailListRecipientService;
 import com.google.gdata.client.appsforyourdomain.EmailListService;
 import com.google.gdata.client.appsforyourdomain.NicknameService;
 import com.google.gdata.client.appsforyourdomain.UserService;
-import com.google.gdata.data.Link;
-import com.google.gdata.data.appsforyourdomain.AppsForYourDomainErrorCode;
 import com.google.gdata.data.appsforyourdomain.AppsForYourDomainException;
-import com.google.gdata.data.appsforyourdomain.EmailList;
 import com.google.gdata.data.appsforyourdomain.Login;
 import com.google.gdata.data.appsforyourdomain.Name;
-import com.google.gdata.data.appsforyourdomain.Nickname;
 import com.google.gdata.data.appsforyourdomain.Quota;
-import com.google.gdata.data.appsforyourdomain.generic.GenericEntry;
-import com.google.gdata.data.appsforyourdomain.generic.GenericFeed;
-import com.google.gdata.data.appsforyourdomain.provisioning.EmailListEntry;
-import com.google.gdata.data.appsforyourdomain.provisioning.EmailListFeed;
-import com.google.gdata.data.appsforyourdomain.provisioning.EmailListRecipientEntry;
-import com.google.gdata.data.appsforyourdomain.provisioning.EmailListRecipientFeed;
-import com.google.gdata.data.appsforyourdomain.provisioning.NicknameEntry;
-import com.google.gdata.data.appsforyourdomain.provisioning.NicknameFeed;
 import com.google.gdata.data.appsforyourdomain.provisioning.UserEntry;
-import com.google.gdata.data.appsforyourdomain.provisioning.UserFeed;
-
-import com.google.gdata.data.extensions.Who;
 import com.google.gdata.util.ServiceException;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.io.Writer;
-import java.lang.reflect.Array;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-
-import javassist.bytecode.ByteArray;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-import javax.validation.Validator;
-
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.codehaus.jackson.JsonParseException;
-import org.junit.internal.matchers.Each;
-import org.megam.mammoth.prov.info.GoogleUser;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.style.ToStringCreator;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.test.web.server.MockMvc;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import static org.springframework.test.web.server.setup.MockMvcBuilders.*;
-import static org.springframework.test.web.server.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.server.result.MockMvcResultHandlers.*;
 
 @Service
 public class GoogleAppsServiceImpl implements GoogleAppsService {
